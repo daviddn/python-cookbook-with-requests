497b6a335a23554914cea67f955084f69a5ab722
David Naim <DNaim@spartaglobal.com>
2019-10-31 09:03:41 +0000

    initial push

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,14 @@
 # Recipe:: default
 #
 # Copyright:: 2019, The Authors, All Rights Reserved.
+
+apt_update 'update_sources' do
+  action :update
+end
+
+package 'python'
+package 'python-pip'
+
+execute 'install requests==2.3.0' do
+  command 'pip install requests==2.3.0'
+end
--- spec/unit/recipes/default_spec.rb
+++ spec/unit/recipes/default_spec.rb
@@ -15,15 +15,21 @@ describe 'python::default' do
     it 'converges successfully' do
       expect { chef_run }.to_not raise_error
     end
-  end
 
-  context 'When all attributes are default, on CentOS 7' do
-    # for a complete list of available platforms and versions see:
-    # https://github.com/chefspec/fauxhai/blob/master/PLATFORMS.md
-    platform 'centos', '7'
+    it 'should update source list' do
+      expect(chef_run).to update_apt_update 'update_sources'
+    end
 
-    it 'converges successfully' do
-      expect { chef_run }.to_not raise_error
+    it 'should install python' do
+      expect(chef_run).to install_package 'python'
+    end
+
+    it 'should install pip' do
+      expect(chef_run).to install_package 'python-pip'
+    end
+
+    it 'should install requests' do
+      expect(chef_run).to run_execute 'install requests==2.3.0'
     end
   end
 end
